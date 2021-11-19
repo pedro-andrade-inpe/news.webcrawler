b376ca5063a71255ac7ab5ddb4f7560c73efae12
pedro-andrade-inpe <pedro.andrade@inpe.br>
2021-11-19 08:08:57 -0300

    Adding startup message.

--- R/zzz.R
+++ R/zzz.R
@@ -3,3 +3,8 @@ utils::globalVariables(c(".", "%>%", ":="))
 
 #' @importFrom magrittr %>%
 NULL
+
+.onAttach <- function(lib, pkg){
+  packageStartupMessage(sprintf("news.webcrawler version %s is now loaded",
+                                utils::packageDescription("news.webcrawler")$Version))
+}
